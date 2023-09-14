b8844ad8fa1f87be2b0161e310355e05410d3f58
krishna9802 <krishna@artenos.io>
2023-09-14 15:45:18 +0530

    install(basic setup):mui-next-typescript installation successful

---
+++ app/theme/theme.tsx
@@ -0,0 +1,14 @@
+import { createTheme } from "@mui/material";
+
+const theme = createTheme({
+    typography:{
+        fontSize:80
+    },
+    palette:{
+        primary:{
+            main:"#ff0000"
+        }
+    }
+})
+
+export {theme}
